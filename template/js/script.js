54734b926245e7ce0e41a4d4610abf29d66b7702
denisgaker <denisgaker@gmail.com>
2020-07-17 18:29:31 +0300

    Запись в DOM разметки с данными по добавленной задаче

--- template/js/script.js
+++ template/js/script.js
@@ -48,10 +48,30 @@ function addNewTask()
             data: dataTask,
             success: function(data)
             {
-                console.log(JSON.parse(data));
+                let newTaskData = JSON.parse(data);
+
+                if($('#noTasks')) $('#noTasks').remove();
+
+                $('#UTcontainer').append(
+                    '<div class="row userTaskWrap">' +
+                        '<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 userInf">' +
+                            '<p class="text-secondary name_p" id="name_p_' + newTaskData.id + '">' +
+                                '<span class="userInf_title">Пользователь: </span>' + newTaskData.name +
+                            '</p>' +
+                            '<p class="text-secondary email_p" id="email_p_' + newTaskData.id + '">' +
+                                '<span class="userInf_title">E-mail: ' + newTaskData.email + '</span>' +
+                        '</div>' +
+                        '<div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 userTask">' +
+                            '<p class="task_p" id="task_p_' + newTaskData.id + '">' +
+                                '<span class="userTask_title">Задача: ' + newTaskData.task + '</span>' +
+                            '</p>' +
+                        '</div>' +
+                    '</div>'
+                );
+
+                $('#name_userInp, #email_userInp, #task_userInp').val("");
+                $('#modal_addNewTask').modal('hide');
             }
         });
     }
-
-    console.log('Данные по задаче: ', dataTask);
 }
